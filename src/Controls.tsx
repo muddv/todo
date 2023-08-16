953f670dbaeb3a9736cc96965503afb42669356f
misha <dedovmu@gmail.com>
2023-08-16 15:57:11 +0300

    Add delete all completed button

--- src/Controls.tsx
+++ src/Controls.tsx
@@ -1,15 +1,19 @@
-import { useSetAtom } from "jotai";
+import { useAtom } from "jotai";
 
 import { todosAtom } from "../stores/todos";
 
 export function Controls() {
-  const setTodos = useSetAtom(todosAtom);
+  const [todos, setTodos] = useAtom(todosAtom);
   function handleNew() {
     const dialog: HTMLDialogElement = document.querySelector(".addNew")!;
     dialog.showModal();
   }
   function deleteAll() {
     setTodos({ data: [], error: "" });
+  }
+  function deleteAllCompleted() {
+    const activeTodos = todos.data.filter((t) => !t.completed);
+    setTodos({ data: activeTodos, error: "" });
   }
   return (
     <div className="my-5 flex gap-5">
@@ -18,6 +22,12 @@ export function Controls() {
         onClick={handleNew}
       >
         Add new
+      </button>
+      <button
+        className="rounded border border-red-700 bg-red-600 px-4 py-2 font-bold text-white hover:bg-red-700"
+        onClick={deleteAllCompleted}
+      >
+        Delete all completed
       </button>
       <button
         className="rounded border border-red-700 bg-red-600 px-4 py-2 font-bold text-white hover:bg-red-700"
